59bb6d138f6aead366ed02fcacf8f5027a2247ca
Larisa <kosareva.larisa@gmail.com>
2024-04-18 20:59:38 +0200

    Hangman first commit

---
+++ pj/src/Hangman.java
@@ -0,0 +1,116 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hangman {
+
+    public static String[] words = { "ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
+            "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
+            "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
+            "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
+            "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon",
+            "python", "rabbit", "ram", "rat", "raven", "rhino", "salmon", "seal",
+            "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
+            "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
+            "wombat", "zebra" };
+
+    public static String[] gallows = { "+---+\n" +
+            "|   |\n" +
+            "    |\n" +
+            "    |\n" +
+            "    |\n" +
+            "    |\n" +
+            "=========\n",
+
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "=========\n",
+
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "|   |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "=========\n",
+
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|   |\n" +
+                    "     |\n" +
+                    "     |\n" +
+                    " =========\n",
+
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|\\  |\n" + // if you were wondering, the only way to print '\' is with a trailing escape
+                                  // character, which also happens to be '\'
+                    "     |\n" +
+                    "     |\n" +
+                    " =========\n",
+
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|\\  |\n" +
+                    "/    |\n" +
+                    "     |\n" +
+                    " =========\n",
+
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|\\  |\n" +
+                    "/ \\  |\n" +
+                    "     |\n" +
+                    " =========\n" };
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String misteryWord = randomWord(words);
+        int numOfUnderscores = misteryWord.length();
+        printPlaceholders(numOfUnderscores);
+        char[] arrOfLetters = misteryWord.toCharArray();
+        
+
+    }
+
+    public static String randomWord(String[] words){
+        Random random = new Random();
+        int chosen = random.nextInt(words.length);
+        return words[chosen];
+    }
+
+    public static char[] printPlaceholders(int num) {
+        char[] placeholders = new char[num];
+        for (int i = 0; i < num; i++) {
+            System.out.print(placeholders[i] + " ");
+        }
+        return placeholders;
+    }
+
+    public static int checkGuess(char guess, char[] arrOfLetters){
+        for(int i = 0; i < arrOfLetters.length; i++){
+            if( arrOfLetters[i] == guess){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static char[] updatePlaceholders(int i, char[] placeholders, char letter){
+        placeholders[i] = letter;
+        return placeholders;
+    }
+
+
+
+    
+
+
+}
