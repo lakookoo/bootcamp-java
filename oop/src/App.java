d330cfa09e58c4e105d691f06096e7b81eba09db
Larisa <kosareva.larisa@gmail.com>
2024-04-19 17:43:10 +0200

    copy constructor, avoid reference trap

---
+++ oop/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args)  {
+       Car nissan = new Car("Nissan", 10000, 2020, "Green");
+
+       Car nissan2 = new Car(nissan);
+    }
+}
---
+++ oop/src/Car.java
@@ -0,0 +1,55 @@
+public class Car {
+    private String make;
+    private double price;
+    private int year;
+    private String color;
+
+    public Car(String make, double price, int year, String color){
+        this.make = make;
+        this.price = price;
+        this.year = year;
+        this.color = color;
+    }
+
+    public Car(Car source){
+        this.make = source.make;
+        this.price = source.price;
+        this.year = source.year;
+        this.color = source.color;
+    }
+
+    public String getMake(){
+        return make;
+    }
+
+    public void setMake(String make){
+        this.make = make;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    
+
+}
