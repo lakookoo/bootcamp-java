22cfa69c25fa7c2f21a9ae9165500ca281107037
Larisa <kosareva.larisa@gmail.com>
2024-04-19 19:39:11 +0200

    wip

--- oop/src/Dealership.java
+++ oop/src/Dealership.java
@@ -9,4 +9,20 @@ public class Dealership {
             this.cars[i] = new Car(cars[i]);
         }
     }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < this.cars.length; i++) {
+            temp += "Parking Spot: " + i + "\n";
+            String carDescription = this.cars[i].toString();
+            temp += carDescription + "\n";
+        }
+        return temp;
+    }
+
+    public void sell(int index){
+        this.cars[index].drive();
+    }
+
+    // it doesn't have sence to have here getters and setters
 }
--- oop/src/Main.java
+++ oop/src/Main.java
@@ -1,5 +1,10 @@
-public class App {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        
         Car[] cars = new Car[] {
             new Car("Nissan", 5000, 2020, "red", new String[] {"tires", "keys"}),
             new Car("Dodge", 8500, 2019, "blue", new String[] {"tires", "keys"}),
@@ -7,7 +12,20 @@ public class App {
             new Car("Honda", 7000, 2019, "orange", new String[] {"tires", "filter"}),
             new Car("Mercedes", 12000, 2015, "jet black", new String[] {"tires", "filter", "transmission"})
         };
-
+        
         Dealership dealership = new Dealership(cars);
+        
+
+        System.out.println("\n ****** JAVA DEALERSHIP! ****** \n");        
+        System.out.println("Feel free to browse through our collection of cars.\n");
+        System.out.println(dealership);
+        System.out.println("Which car are you interested in? (0 – 4).\n");
+        int index = scan.nextInt();
+
+        dealership.sell(index);
+
+
+        scan.close();
+
     }
 }
