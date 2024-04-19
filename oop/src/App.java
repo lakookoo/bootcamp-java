8d4ff2f8fba100fec052dcd33d8d612e8898fb88
Larisa <kosareva.larisa@gmail.com>
2024-04-19 19:29:07 +0200

    deep copying, array of objects

--- oop/src/App.java
+++ oop/src/App.java
@@ -1,13 +1,13 @@
 public class App {
     public static void main(String[] args) {
-        String[] spareParts = new String[] { "Tires", "Keys" };
-        Car nissan = new Car("Nissan", 10000, 2020, "Green", spareParts);
-        Car dodge = new Car("Dodge", 11000, 2021, "Greenish", spareParts);
-        spareParts[0] = "Filter";
-        Car nissan2 = new Car(nissan);
+        Car[] cars = new Car[] {
+            new Car("Nissan", 5000, 2020, "red", new String[] {"tires", "keys"}),
+            new Car("Dodge", 8500, 2019, "blue", new String[] {"tires", "keys"}),
+            new Car("Nissan", 5000, 2017, "yellow", new String[] {"tires", "filter"}),
+            new Car("Honda", 7000, 2019, "orange", new String[] {"tires", "filter"}),
+            new Car("Mercedes", 12000, 2015, "jet black", new String[] {"tires", "filter", "transmission"})
+        };
 
-        System.out.println(dodge);
-        System.out.println(nissan);
-        System.out.println(nissan2);
+        Dealership dealership = new Dealership(cars);
     }
 }
---
+++ oop/src/Dealership.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Dealership {
+    private Car[] cars;
+
+    public Dealership(Car[] cars){
+        this.cars = new Car[cars.length];
+        for (int i = 0; i < this.cars.length; i++){
+            this.cars[i] = new Car(cars[i]);
+        }
+    }
+}
