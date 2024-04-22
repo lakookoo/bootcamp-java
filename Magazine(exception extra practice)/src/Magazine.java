0410a55d126e6330ab1e90694c35582c21d47de2
Larisa <kosareva.larisa@gmail.com>
2024-04-22 12:07:33 +0200

    add setters and constructor with exception throwing

---
+++ Magazine(exception extra practice)/src/Magazine.java
@@ -0,0 +1,60 @@
+public class Magazine {
+    private String title;
+    private String publisher;
+    private int issueNumber;
+    private int publicationYear;
+
+    public Magazine(String title, String publisher, int issueNumber, int publicationYear){
+        setTitle(title);
+        setPublisher(publisher);
+        setIssueNumber(issueNumber);
+        setPublicationYear(publicationYear);
+    }
+
+    public Magazine(Magazine source){
+        this.title = source.title;
+        this.publisher = source.publisher;
+        this.issueNumber = source.issueNumber;
+        this.publicationYear = source.publicationYear;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        if (title == null || title.isBlank()){
+            throw new IllegalArgumentException("Title cannot be null or blank");
+        }
+    }
+
+    public String getPublisher(){
+        return publisher;
+    }
+
+    public void setPublisher(String publisher){
+        if (publisher == null || publisher.isBlank()){
+            throw new IllegalArgumentException("Publisher cannot be null or blank");
+        }
+    }
+
+    public int getIssueNumber(){
+        return issueNumber;
+    }
+
+    public void setIssueNumber(int issueNumber){
+        if (issueNumber <= 0) {
+            throw new IllegalArgumentException("issue number cannot be equal zero or negative");
+        }
+    }
+
+    public int getPublicationYear(){
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear){
+        if (publicationYear <= 0) {
+            throw new IllegalArgumentException("PublicationYear cannot be equal zero or negative");
+        }
+    }
+}
