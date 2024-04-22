47712d1a9f659ef3550abd9786dfe7ff588e4ed9
Larisa <kosareva.larisa@gmail.com>
2024-04-22 10:29:25 +0200

    exceptions

---
+++ Exceptions/src/App.java
@@ -0,0 +1,18 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        try { 
+            FileInputStream fis = new FileInputStream("greetings.txt"); 
+            Scanner scanner = new Scanner(fis);
+            System.out.println(scanner.nextLine());
+            scanner.close();
+        } catch (FileNotFoundException exception){
+            System.out.println(exception.getMessage());
+        } finally {
+            System.out.println("Process complete");
+        }
+    }
+}
