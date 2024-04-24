92642678c235bdc0975ad3b76156a5d248153d89
Larisa <kosareva.larisa@gmail.com>
2024-04-24 18:18:51 +0200

    static, final, static import

---
+++ StaticAndFinal/src/staticImport/Circle.java
@@ -0,0 +1,39 @@
+package staticImport;
+
+// import static java.lang.Math.PI;
+// import static java.lang.Math.max;
+import static java.lang.Math.*;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getCircumference() {
+        return 2 * PI * radius;
+    }
+
+    public double getArea() {
+        return PI * radius * radius;
+    }
+
+    public double getLargerRadius(double otherRadius) {
+        return max(radius, otherRadius);
+    }
+
+    public double getSmallerRadius(double otherRadius) {
+        return min(radius, otherRadius);
+    }
+
+    public double getRadiusSquared() {
+        return pow(radius, 2);
+    }
+
+    public double getRadiusSquareRoot() {
+        return sqrt(radius);
+    }
+
+}
