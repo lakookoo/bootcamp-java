9677ef9d1d8ad9ca45a935ab2f196740b40db0ce
Larisa <kosareva.larisa@gmail.com>
2024-04-24 16:07:03 +0200

    fix bug in a copy constructor

--- CityPopulationTracker(Map collections)/src/City.java
+++ CityPopulationTracker(Map collections)/src/City.java
@@ -2,53 +2,49 @@ public class City {
     private String name;
     private String country;
     private long population;
-    
+
     public City(String name, String country, long population) {
         setName(name);
         setCountry(country);
         setPopulation(population);
     }
-
+    
     public City(City source) {
-        source.setName(name);
-        source.setCountry(country);
-        source.setPopulation(population);
+        setName(source.getName());
+        setCountry(source.getCountry());
+        setPopulation(source.getPopulation());
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public void setName(String name){
-        if(name == null || name.isBlank()){
-            throw new IllegalArgumentException("City name cannot be null or blank");
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or blank.");
         }
         this.name = name;
     }
 
-    public String getCountry(){
-        return country;
+    public String getCountry() {
+        return this.country;
     }
 
-    public void setCountry(String country){
-        if(country == null || country.isBlank()){
-            throw new IllegalArgumentException("Country cannot be null or blank");
+    public void setCountry(String country) {
+        if (country == null || country.isBlank()) {
+            throw new IllegalArgumentException("Country cannot be null or blank.");
         }
         this.country = country;
     }
 
-    public long getPopulation(){
-        return population;
+    public long getPopulation() {
+        return this.population;
     }
 
-    public void setPopulation(long population){
-        if(population <= 0){
-            throw new IllegalArgumentException("population must be greater than 0");
+    public void setPopulation(long population) {
+        if (population <= 0) {
+            throw new IllegalArgumentException("Population must be greater than 0.");
         }
         this.population = population;
     }
-
-    
-    
-
 }
