565afdfbc94cb38926ff4cf379503be38ae9c6cb
Larisa <kosareva.larisa@gmail.com>
2024-04-25 14:25:40 +0200

    add dealership class and test it

--- CarDealership/src/App.java
+++ CarDealership/src/App.java
@@ -1,38 +1,36 @@
 import model.Car;
+import model.CarDealership;
 import model.Car.BodyType;
 
 public class App {
     public static void main(String[] args) {
         Car car1 = new Car("Toyota", "Camry", BodyType.SEDAN, 2020, 30000);
-        System.out.println(car1.getMake());
+        Car car2 = new Car("Honda", "Civic", BodyType.SEDAN, 2021, 25000);
     
     
-        // Uncomment the following line to test invalid input for setMake method
-        //car1.setMake("");
+        CarDealership dealership = new CarDealership();
+        
+        // Test the addCar method
+        dealership.addCar(car1);
+        dealership.addCar(car2);
     
     
-        // Uncomment the following line to test invalid input for setModel method
-        // car1.setModel("");
+        // Test the getCar method
+        Car retrievedCar = dealership.getCar(0);
+        System.out.println(retrievedCar.getMake() + " " + retrievedCar.getModel());
     
     
-        // Uncomment the following line to test invalid input for setBodyType method
-        // car1.setBodyType(null);
+        // Test the setCar method
+        Car newCar = new Car("Ford", "Mustang", BodyType.COUPE, 2022, 45000);
+        dealership.setCar( 0, newCar);
     
     
-        // Uncomment the following line to test invalid input for setProductionYear method
-        // car1.setProductionYear(1899);
-    
-    
-        // Uncomment the following line to test invalid input for setPrice method
-        // car1.setPrice(-1);
-    
-    
-        // Uncomment the following line to test invalid input for setPrice method
-        // car1.setPrice(200001);
-    
-    
-        // Uncomment the following line to test invalid input for the constructor
-        // Car car2 = new Car("", "Camry", BodyType.SEDAN, 2020, 30000);
+        // Verify that the car was updated
+        retrievedCar = dealership.getCar(0);
+        System.out.println(retrievedCar.getMake() + " " + retrievedCar.getModel());
     }
+    
+    
+    
     
 }
--- CarDealership/src/model/CarDealership.java
+++ CarDealership/src/model/CarDealership.java
@@ -1,5 +1,23 @@
 package model;
 
+import java.util.ArrayList;
+
 public class CarDealership {
-    
+    ArrayList<Car> cars;
+
+    public CarDealership(){
+        this.cars = new ArrayList<>();
+    }
+
+    public Car getCar(int index){
+        return new Car(this.cars.get(index));
+    }
+
+    public void setCar(int index, Car car){
+        this.cars.set(index, new Car(car));
+    }
+
+    public void addCar(Car car){
+        this.cars.add(new Car(car));
+    }
 }
