f6f18fdec3face39997f4a183666e85a0bbd1535
Larisa <kosareva.larisa@gmail.com>
2024-05-06 13:08:41 +0200

    interface

---
+++ Interface/starter-project/service/CreditService.java
@@ -0,0 +1,29 @@
+package service;
+
+import repository.AccountRepository;
+import pojo.Credit;
+
+public class CreditService {
+
+    AccountRepository accountRepository;
+
+    public CreditService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+    public void createAccount(Credit account) {
+        this.accountRepository.createAccount(account);;
+    }
+
+    public Credit retrieveAccount(String id) {
+        return (Credit)this.accountRepository.retrieveAccount(id);
+    }
+
+    public void updateAccount(Credit account) {
+        this.accountRepository.updateAccount(account);;
+    }
+
+    public void deleteAccount(String id) {
+        this.accountRepository.deleteAccount(id);;
+    }
+
+}
